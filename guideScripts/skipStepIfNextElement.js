7783cab9d3c4d58f8c54b7b232246254c3859bdf
Adam Chasen <adam@pendo.io>
2015-11-11 16:28:04 -0500

    michael's enhancements to guidescript after investigating double eval timing issue research

--- guideScripts/skipStepIfNextElement.js
+++ guideScripts/skipStepIfNextElement.js
@@ -1,26 +1,55 @@
 pendo.Sizzle('._pendo-guide_')[0].style['display'] = 'none';
-// Only for lightboxes
-//pendo.Sizzle('._pendo-backdrop_')[0].style['display'] = 'none';
-pendo._.defer(function (){
+//TODO wrap this in a try
+pendo.Sizzle('._pendo-backdrop_')[0].style['display'] = 'none';
+(function() {
+    // need to know:
+    // -- this steps' index
+    // get these as soon as the step renders, otherwise timing issues can give us the wrong step object
+    var activeObj = getActiveGuide();
+    var nextStep = activeObj.guide.steps[activeObj.stepIndex + 1];
+
+pendo._.defer(function(){setTimeout(function (){
 
     // Test next step's element to see if it is present
     // if it's not, skip that step.
 
-    var activeObj = pendo.guideDev.getActiveGuide() 
-    var nextStep = activeObj.guide.steps[activeObj.stepIndex + 1];
-
     var results = pendo.Sizzle(nextStep.elementPathRule);
-    if (results.length == 0 || !pendo._.some(results, pendo.isElementVisible)) {
-        // in this case we want:
-        // 
-        // - to show this guide + backdrop
-        // - rig up the advance method to complete the guide
-
-        pendo.Sizzle('._pendo-guide_')[0].style['display'] = 'block';
-        // Only for lightboxes
-        //pendo.Sizzle('._pendo-backdrop_')[0].style['display'] = 'block';
-
+    // this check may not be necessary, but for good measure
+    if (activeObj.step.seenState == 'advanced' || activeObj.step.seenState == 'dismissed') {
+      return;//Step already advanced, do nothing
+    } else if (results.length == 0 || !pendo._.some(results, pendo.isElementVisible)) {
+        // in this case we want to skip 2 steps forward
+        pendo.log('advance 2 steps');
+        pendo.onGuideAdvanced({steps: 2});
     } else {
-        pendo.onGuideAdvanced(activeObj.step);
+      pendo.log('advance 1 step');
+      pendo.onGuideAdvanced(activeObj.step);
     }
-});
+}, 1000)});
+
+})();
+
+// TODO find out why pendo.guidDev version doesn't always work
+// may be related to the previous version before timing fix
+function getActiveGuide (){
+    var currentStep = null, stepIdx = -1;
+    var currentGuide = pendo._.find(pendo.guides, function(guide) { 
+        return pendo._.find(guide.steps, function(step, idx){
+            var id_str = "_pendo_g_"+step.id;
+            if (pendo.Sizzle('#'+id_str).length > 0) {
+                currentStep = step;
+                stepIdx = idx;
+                return true;
+            }
+            return false;
+        });
+    });
+
+    if (!currentGuide) return null;
+
+    return {
+        guide: currentGuide,
+        step: currentStep,
+        stepIndex: stepIdx
+    };
+};
