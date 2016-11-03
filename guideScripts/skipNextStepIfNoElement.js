23395803bb6253cc6c328d78f9beee4cf9e26595
Adam Chasen <adam@chasen.name>
2016-11-03 13:45:36 -0400

    improvements to guide lookup and testing for element

--- guideScripts/skipNextStepIfNoElement.js
+++ guideScripts/skipNextStepIfNoElement.js
@@ -1,12 +1,19 @@
 //TODO wrap this in a try
+// CSS for banner: ._pendo-guide-container_ { display: none;}
+
 pendo.Sizzle('._pendo-guide_')[0].style['display'] = 'none';
 pendo.Sizzle('._pendo-backdrop_')[0].style['display'] = 'none';
+
 (function() {
     // need to know:
     // -- this steps' index
     // get these as soon as the step renders, otherwise timing issues can give us the wrong step object
-    var activeObj = pendo.guideDev.getActiveGuide();
-    var nextStep = activeObj.guide.steps[activeObj.stepIndex + 1];
+    var guideId = "<%= guide.id %>";
+    var stepId = "<%= step.id %>";
+    var guide = pendo.findGuideById(guideId);
+    var step = guide.findStepById(stepId);
+    var nextStepIndex = pendo._.indexOf(guide.steps, step) + 1;
+    var nextStep = guide.steps[nextStepIndex];
 
 setTimeout(function (){
 
@@ -14,16 +21,13 @@ setTimeout(function (){
     // if it's not, skip that step.
 
     var results = pendo.Sizzle(nextStep.elementPathRule);
-    // this check may not be necessary, but for good measure
-    if (activeObj.step.seenState == 'advanced' || activeObj.step.seenState == 'dismissed') {
-      return;//Step already advanced, do nothing
-    } else if (results.length == 0 || !pendo._.some(results, pendo.isElementVisible)) {
+    if (results.length == 0 || !pendo._.some(results, pendo.isElementVisible)) {
         // in this case we want to skip 2 steps forward
         pendo.log('advance 2 steps');
-        pendo.onGuideAdvanced({steps: 2});
+        pendo.onGuideAdvanced(nextStep);
     } else {
       pendo.log('advance 1 step');
-      pendo.onGuideAdvanced(activeObj.step);
+      pendo.onGuideAdvanced(step);
     }
 }, 1000);
 
